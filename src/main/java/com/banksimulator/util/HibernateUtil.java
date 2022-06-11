1f755840f71a9978e46568b9e9200c3adcfe1682
Eshghialireza <es.alrza@gmail.com>
2022-06-12 01:53:32 +0430

    HibernateUtil in single tone mode with entityManager method

--- src/main/java/com/banksimulator/util/HibernateUtil.java
+++ src/main/java/com/banksimulator/util/HibernateUtil.java
@@ -1,14 +1,25 @@
 package com.banksimulator.util;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class HibernateUtil {
-    public static final EntityManagerFactory emf;
+    private static final EntityManagerFactory emf;
+
     static{
         emf= Persistence.createEntityManagerFactory("default");
     }
-    public static EntityManagerFactory getEmf(){
-        return emf;
+
+    private static EntityManager em;
+
+    private HibernateUtil(){
+    }
+
+    public static EntityManager getEm() {
+        if(em==null){
+            em=emf.createEntityManager();
+        }
+        return em;
     }
 }
